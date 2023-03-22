d419d7dab04177c7f5a1cfcadc2378643f19c8fc
Sara <saritaprieto445@gmail.com>
2023-03-21 22:01:13 -0500

    intento 1 ajax

---
+++ pruebadosAjax-Springboot/src/main/java/com/example/pruebadosAjaxSpringboot/PruebadosAjaxSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.example.pruebadosAjaxSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebadosAjaxSpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PruebadosAjaxSpringbootApplication.class, args);
+	}
+
+}
